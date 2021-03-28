5f44b6fd6df473f5bd34c927ce2eba5c098af00c
ropapermaker <58087674+ropapermaker@users.noreply.github.com>
2021-03-28 21:18:04 +0200

    Add files via upload
    added files

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        darkpurple: { DEFAULT: '#4B344E' },
+        darkblue: { DEFAULT: '#081426' },
+      }
+    }
+  }
+}
